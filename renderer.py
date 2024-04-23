c984c6c2d6768936e779dfc19d0abca61e6bdecc
Lincoln <MrSmartyMcFarty@gmail.com>
2024-04-23 07:37:46 -0500

    stuff

--- renderer.py
+++ renderer.py
@@ -11,7 +11,7 @@ matWidth = 32
 p = Pin(pinNum, Pin.OUT)
 np = neopixel.NeoPixel(p, matHeight * matWidth)
 
-#letterDicts = genericFont()
+# letterDicts = genericFont()
 
 word = "Hello"
 
@@ -19,34 +19,44 @@ word = "Hello"
 #     print(key)
 #     print(value)
 
+
 def renderLetter(letter, index):
     t = genericFont.getSymbol(letter)
     t = t.split("\n")
     maxWidth = 0
     for x in range(len(t)):
         for y in range(len(t[x])):
-
             if len(t[x]) > maxWidth:
                 maxWidth = len(t[x])
+            if y + index < matWidth and y + index >= 0:
 
-            if (y+index) % 2 == 0:
-                if t[x][y] == "#":
-                    np[x + (y+index) * matHeight] = (255, 0, 0)
+                if (y+index) % 2 == 0:
+                    if t[x][y] == "#":
+                        np[x + (y+index) * matHeight] = (255, 0, 0)
+                    else:
+                        np[x + (y+index) * matHeight] = (0, 0, 0)
                 else:
-                    np[x + (y+index) * matHeight] = (0, 0, 0)
-            else:
-                if t[x][y] == "#":
-                    np[(y+1+index) * matHeight - x-1] = (255, 0, 0)
-                else:
-                    np[(y+1+index) * matHeight-x-1] = (0, 0, 0)
+                    if t[x][y] == "#":
+                        np[(y+1+index) * matHeight - x-1] = (255, 0, 0)
+                    else:
+                        np[(y+1+index) * matHeight-x-1] = (0, 0, 0)
     return maxWidth
 
 
-def renderWord(word):
-    index = 0
+def clear():
+    for x in range(matWidth * matHeight):
+        np[x] = (0, 0, 0)
+
+
+def renderWord(word, index):
     for letter in list(word):
         index += renderLetter(letter, index) + 1
     np.write()
 
 
-renderWord("MALL")
+i = 0
+while i < 1000:
+    clear()
+    renderWord(".000001001", 0 - i)
+    time.sleep(0.5)
+    i += 1
